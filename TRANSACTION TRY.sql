4feede9c9a3e82107044a953b248a36839aee929
Renan Valentim <rn.oliveira95@gmail.com>
2021-02-09 12:35:07 -0300

    Add files via upload

---
+++ TRANSACTION TRY.sql
@@ -0,0 +1,26 @@
+BEGIN TRY
+	BEGIN TRANSACTION
+	
+	--> [YOUR QUERY HERE] <--
+
+	-- Commands End
+	COMMIT TRAN -- Transaction Success!
+END TRY
+BEGIN CATCH
+	IF @@TRANCOUNT > 0
+		ROLLBACK TRAN --RollBack in case of Error
+
+	-- Raise ERROR with RAISEERROR() Statement including the details of the exception
+	DECLARE @ErrorLine INT;
+	DECLARE @ErrorMessage NVARCHAR(4000);
+	DECLARE @ErrorSeverity INT;
+	DECLARE @ErrorState INT;
+
+	SELECT
+		@ErrorLine = ERROR_LINE(),
+		@ErrorMessage = ERROR_MESSAGE(),
+		@ErrorSeverity = ERROR_SEVERITY(),
+		@ErrorState = ERROR_STATE();
+		 
+	RAISERROR ('Error found in line %i: %s', @ErrorSeverity, @ErrorState, @ErrorLine, @ErrorMessage) WITH SETERROR
+END CATCH
